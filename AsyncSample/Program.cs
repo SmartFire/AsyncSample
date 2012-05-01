0dd39439a7e8b8c14af731a689ca94fbb3a809d0
Emre Tekelioglu <emre.tekelioglu@4primes.com>
2012-05-01 17:03:14 +0300

    initial commit

---
+++ AsyncSample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AsyncExample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] files = { "test1", "test2", "test3" };
+            Task[] tasks = new Task[5];
+            for (int i = 0; i < 5; i++)
+            {
+                tasks[i] = new Task(i);
+                tasks[i].MyTaskAsync(files);
+                tasks[i].MyTaskCompleted += new EventHandler<MyTaskAsyncCompletedEventArgs>(task_MyTaskCompleted);
+                tasks[i].MyTaskProgressChanged += new EventHandler<MyTaskProgressChangedEventArgs>(task_MyTaskProgressChanged);
+                Thread.Sleep(300);
+            }
+
+            Thread.Sleep(1000);
+
+            for (int i = 0; i < 5; i++)
+            {
+                tasks[i].CancelAsync();
+            }
+
+            Thread.Sleep(3000);
+            Console.ReadLine();
+        }
+
+        static void task_MyTaskProgressChanged(object sender, MyTaskProgressChangedEventArgs e)
+        {
+            Console.WriteLine("[MyTask][{2}] Progress: {0} %, Current file: {1}", e.ProgressPercentage, e.CurrentFile, e.Id);
+        }
+
+        static void task_MyTaskCompleted(object sender, MyTaskAsyncCompletedEventArgs e)
+        {
+            Console.WriteLine("Main : task[{0}] completed :) ", e.Id);
+        }
+    }
+}
---
+++ AsyncSample/Task.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.ComponentModel;
+using System.Runtime.Remoting.Messaging;
+
+namespace AsyncExample
+{
+    internal class MyAsyncContext
+    {
+        private readonly object _sync = new object();
+        private bool _isCancelling = false;
+
+        public bool IsCancelling
+        {
+            get
+            {
+                lock (_sync) { return _isCancelling; }
+            }
+        }
+
+        public void Cancel()
+        {
+            lock (_sync) { _isCancelling = true; }
+        }
+    }
+
+    public class MyTaskProgressChangedEventArgs : ProgressChangedEventArgs
+    {
+        private string _currentFile;
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public string CurrentFile
+        {
+            get { return _currentFile; }
+        }
+
+        public MyTaskProgressChangedEventArgs(int progressPercentage, string currentFile, int Id,
+          object userState)
+            : base(progressPercentage, userState)
+        {
+            _currentFile = currentFile;
+            _id = Id;
+        }
+    }
+
+    public class MyTaskAsyncCompletedEventArgs : AsyncCompletedEventArgs
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public MyTaskAsyncCompletedEventArgs(Exception error, bool cancelled, object userState, int Id)
+            : base(error, cancelled, userState)
+        {
+            _id = Id;
+        }
+    }
+
+    class Task
+    {
+        private delegate void MyTaskWorkerDelegate(string[] files, AsyncOperation async, MyAsyncContext asyncContext, out bool cancelled);
+
+        private readonly object _sync = new object();
+        private bool _myTaskIsRunning = false;
+
+        public event EventHandler<MyTaskAsyncCompletedEventArgs> MyTaskCompleted;
+        public event EventHandler<MyTaskProgressChangedEventArgs> MyTaskProgressChanged;
+
+        private MyAsyncContext _myTaskContext = null;
+
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public Task(int id)
+        {
+            Id = id;
+        }
+
+
+        public bool IsBusy
+        {
+            get { return _myTaskIsRunning; }
+        }
+
+        private void MyTaskWorker(string[] files, AsyncOperation async, MyAsyncContext asyncContext, out bool cancelled)
+        {
+            cancelled = false;
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                // a time consuming operation with a file (compression, encryption etc.)
+                Thread.Sleep(1000);
+
+                // compute progress
+                int progressPercentage = 100 * (i + 1) / files.Length;
+
+                // raise the progress changed event
+                MyTaskProgressChangedEventArgs eArgs = new MyTaskProgressChangedEventArgs(progressPercentage, files[i], Id, null);
+                async.Post(delegate(object e) { OnMyTaskProgressChanged((MyTaskProgressChangedEventArgs)e); }, eArgs);
+
+                if (asyncContext.IsCancelling)
+                {
+                    cancelled = true;
+                    return;
+                }
+            }
+        }
+
+        protected virtual void OnMyTaskProgressChanged(MyTaskProgressChangedEventArgs e)
+        {
+            if (MyTaskProgressChanged != null)
+                MyTaskProgressChanged(this, e);
+        }
+
+        public void MyTaskAsync(string[] files)
+        {
+            MyTaskWorkerDelegate worker = new MyTaskWorkerDelegate(MyTaskWorker);
+            AsyncCallback completedCallback = new AsyncCallback(MyTaskCompletedCallback);
+
+            lock (_sync)
+            {
+                if (_myTaskIsRunning)
+                    throw new InvalidOperationException("The control is currently busy.");
+
+                AsyncOperation async = AsyncOperationManager.CreateOperation(null);
+                MyAsyncContext context = new MyAsyncContext();
+                bool cancelled;
+
+                worker.BeginInvoke(files, async, context, out cancelled, completedCallback, async);
+
+                _myTaskIsRunning = true;
+                _myTaskContext = context;
+            }
+        }
+
+        private void MyTaskCompletedCallback(IAsyncResult ar)
+        {
+            // get the original worker delegate and the AsyncOperation instance
+            MyTaskWorkerDelegate worker =
+                (MyTaskWorkerDelegate)((AsyncResult)ar).AsyncDelegate;
+            AsyncOperation async = (AsyncOperation)ar.AsyncState;
+            bool cancelled;
+
+            // finish the asynchronous operation
+            worker.EndInvoke(out cancelled, ar);
+
+            // clear the running task flag
+            lock (_sync)
+            {
+                _myTaskIsRunning = false;
+                _myTaskContext = null;
+            }
+
+            // raise the completed event
+            MyTaskAsyncCompletedEventArgs completedArgs = new MyTaskAsyncCompletedEventArgs(null,
+              cancelled, null, Id);
+            async.PostOperationCompleted(
+              delegate(object e) { OnMyTaskCompleted((MyTaskAsyncCompletedEventArgs)e); },
+              completedArgs);
+        }
+
+        protected virtual void OnMyTaskCompleted(MyTaskAsyncCompletedEventArgs e)
+        {
+            if (MyTaskCompleted != null)
+                MyTaskCompleted(this, e);
+        }
+
+        public void CancelAsync()
+        {
+            lock (_sync)
+            {
+                if (_myTaskContext != null)
+                    _myTaskContext.Cancel();
+            }
+        }
+
+    }
+}
